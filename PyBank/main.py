7259b10965549d838532fb6c91366f0324faab7c
Miguel Eamiguel <mgeamiguel@gmail.com>
2020-11-10 02:42:37 -0800

    Final Draft

--- PyBank/main.py
+++ PyBank/main.py
@@ -66,3 +66,24 @@ with open('Analysis.txt', 'w') as text:
     text.write(f"Average Change: $ round(int(avg_profitloss))\n")
     text.write(f"Greatest Increase in Profits: {best_month} ${highest_change}\n")
     text.write(f"Greatest Decrease in Losses: {worst_month} ${lowest_change}\n")
+
+
+#Your task is to create a Python script that analyzes the records to calculate each of the following:
+
+
+#The total number of months included in the dataset
+
+
+#The net total amount of "Profit/Losses" over the entire period
+
+
+#The average of the changes in "Profit/Losses" over the entire period
+
+
+#The greatest increase in profits (date and amount) over the entire period
+
+
+#The greatest decrease in losses (date and amount) over the entire period
+
+
+#In addition, your final script should both print the analysis to the terminal and export a text file with the results.
